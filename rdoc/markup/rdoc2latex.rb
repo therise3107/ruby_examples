2eec49c687fbca7fc84d1d5ff9433c029f877610
therise3107 <lyadav3107@gmail.com>
2014-11-01 21:51:01 +0530

    initial commit ^
    cit
    exit
    wq
    wq
    :wq

---
+++ rdoc/markup/rdoc2latex.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby1.9.1
+# Illustration of a script to convert an RDoc-style file to a LaTeX document
+
+require 'rdoc/markup'
+require 'rdoc/markup/to_latex'
+
+p = RDoc::Markup.new
+h = RDoc::Markup::ToLaTeX.new
+
+#puts "\\documentclass{report}"
+#puts "\\usepackage{tabularx}"
+#puts "\\usepackage{parskip}"
+#puts "\\begin{document}"
+puts p.convert(ARGF.read, h)
+#puts "\\end{document}"
